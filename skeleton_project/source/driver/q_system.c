ba4017e1289d3d905f2b52c914a3662b0c26d649
Signe Naustan Hjerting <signenh@stud.ntnu.no>
2026-02-26 15:50:49 +0100

    added files for floorpanel module

--- skeleton_project/source/driver/q_system.c
+++ skeleton_project/source/driver/q_system.c
@@ -8,7 +8,15 @@ void q_system_empty_q(struct Q_system *q) {
     };
 }
 
-void q_system_make_q(struct Q_system *q) {
+void q_system_remove_FLR(struct Q_system *q, struct Elevator *elevator) {
+    if (elevator->current_floor == 0) {
+        elevator.BTN_1 = 0;
+        BTN_FLR_4_DWN = 0;
+    }
+}
+
+
+void q_system_make_q(struct Q_system *q, struct Elevator elevator) {
     //Bestillinger nedover
     if (BTN_FLR_2_DWN == true) {
         q->q_down[1] = 1;
@@ -32,5 +40,28 @@ void q_system_make_q(struct Q_system *q) {
     };
 
     //Fiks for knappene inne i heisen
-    
+    if (elevator.BTN_4 == true) {
+        if (elevator.current_floor < 3) { //Hvis man skal til fjerde etasje fra en lavere etasje
+            q->q_up[3] = 1;
+        }
+    }
+    if (elevator.BTN_3 == true) {
+        if (elevator.current_floor < 2) { //Hvis man skal til tredje etasje fra en lavere etasje
+            q->q_up[2] = 1;
+        } else if (elevator.current_floor > 2) { //Hvis man skal til tredje etasje fra en høyere etasje
+            q->q_down[2] = 1;
+        }
+    }
+    if (elevator.BTN_2 == true) {
+        if (elevator.current_floor < 1) { //Hvis man skal til andre etasje fra en lavere etasje
+            q->q_up[1] = 1;
+        } else if (elevator.current_floor > 1) { //Hvis man skal til andre etasje fra en høyere etasje
+            q->q_down[1] = 1;
+        }
+    }
+    if (elevator.BTN_1 == true) {
+        if (elevator.current_floor > 0) { //Hvis man skal til første etasje fra en høyere etasje
+            q->q_down[0] = 1;
+        }
+    }
 }
--- skeleton_project/source/driver/q_system.h
+++ skeleton_project/source/driver/q_system.h
@@ -13,7 +13,7 @@ void q_system_empty_q(struct Q_system *q);
 
 //Funksjon som setter alle bestillinger i en etasje til 0
 //For når heisen ankommer en etasje
-void q_system_remove_FLR(struct Q_system *q);
+void q_system_remove_FLR(struct Q_system *q, struct Elevator *elevator);
 
 //Funksjon som setter bestillinger til 1
-void q_system_make_q(struct Q_system *q);
+void q_system_make_q(struct Q_system *q, struct Elevator elevator);
