a497bda3ac2d32e37bd3c181e44b683753e4cc10
Signe Naustan Hjerting <signenh@stud.ntnu.no>
2026-03-05 13:41:44 +0100

    Fixed some small things

--- skeleton_project/source/driver/q_system.c
+++ skeleton_project/source/driver/q_system.c
@@ -82,21 +82,33 @@ int q_system_get_target_floor(struct Q_system q, struct Elevator elevator) {
     switch (elevator.cur_dir) {
         case -1: //Heisen beveger seg nedover
             //Sjekk hvor vi er og ta neste i rekken som er i kø
-            for (int i = elevator.current_floor; int i = 0; i--) {
+            for (int i = elevator.current_floor; i = 0; i--) {
                 if (q.q_down[i] == 1) { //Sjekker om etasjen er i heissystemet
                     return i; //Hvis den er der returneres etasjen
                 }
             }
             break;
-        case 1; //Heisen beveger seg oppover
-            for (int i = elevator.current_floor; int i = 3; i++) {
+        case 1: //Heisen beveger seg oppover
+            for (int i = elevator.current_floor; i = 3; i++) {
                 if (q.q_up[i] == 1) {
                     return i;
                 }
             }
             break;
-        case 0; //Heisen står stille
-            //Prioritering på køene? Kjører endten til høyeste etasje som skal ned eller laveste etasje som skal opp
+        case 0: //Heisen står stille
+            //Sjekker køen ned
+            for (int i = elevator.current_floor; i = 0; i--) {
+                if (q.q_down[i] == 1) { //Sjekker om etasjen er i heissystemet
+                    return i; //Hvis den er der returneres etasjen
+                }
+            }
+            //Sjekker køen opp
+            for (int i = elevator.current_floor; i = 3; i++) {
+                if (q.q_up[i] == 1) {
+                    return i;
+                }
+            }
+            break;
     }
     return -1; //Heisen fant ikke noe måletasje 
 }
