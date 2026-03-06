d992549150adc98a4a294ab9343d4495bbba3fbb
Signe Naustan Hjerting <signenh@stud.ntnu.no>
2026-03-06 15:45:11 +0100

    Updated the lights module. Got rid of the struct

--- skeleton_project/source/driver/Lights.c
+++ skeleton_project/source/driver/Lights.c
@@ -1,62 +1,119 @@
 #include "Lights.h"
 
 
-struct Lights* Lights_init() {
-    struct Lights* lights;
-    //Initialiserer alle verdiene
-    
-    //Bestillingsknappene
-    lights->BTN_FLR_1_UP = 0;
-    lights->BTN_FLR_2_UP = 0;
-    lights->BTN_FLR_2_DWN = 0;
-    lights->BTN_FLR_3_UP = 0;
-    lights->BTN_FLR_3_DWN = 0;
-    lights->BTN_FLR_4_DWN = 0;
+void Lights_init() {
+    //Bestillingsknapp
+    elevio_buttonLamp(0, BUTTON_HALL_UP, 0); //Setter bestillingsknapplys i første etasje til 0, altså av
+    elevio_buttonLamp(1, BUTTON_HALL_DOWN, 0);
+    elevio_buttonLamp(1, BUTTON_HALL_UP, 0);
+    elevio_buttonLamp(2, BUTTON_HALL_DOWN, 0);
+    elevio_buttonLamp(2, BUTTON_HALL_UP, 0);
+    elevio_buttonLamp(3, BUTTON_HALL_DOWN, 0);
+    elevio_buttonLamp(3, BUTTON_HALL_UP, 0);
+    elevio_buttonLamp(4, BUTTON_HALL_DOWN, 0);
 
-    //Etasjelysene
-    lights->floorLamp_1 = 0;
-    lights->floorLamp_2 = 0;
-    lights->floorLamp_3 = 0;
-    lights->floorLamp_4 = 0;
+    //Etasjelysene settes med elevio_floorIndicator(int floor);
 
     //Panellysene
-    lights->BTN_1 = 0;
-    lights->BTN_2 = 0;
-    lights->BTN_3 = 0;
-    lights->BTN_4 = 0;
+    elevio_buttonLamp(0, BUTTON_CAB, 0);
+    elevio_buttonLamp(1, BUTTON_CAB, 0);
+    elevio_buttonLamp(2, BUTTON_CAB, 0);
+    elevio_buttonLamp(3, BUTTON_CAB, 0);
 
     //Dør åpen lys
-    lights->doorOpenLamp = 0;
+    elevio_doorOpenLamp(0);
 
     //Stopplys
-    lights->stopLamp = 0;
-
-    return lights;
+    elevio_stopLamp(0);
 }
 
 
 
-void Lights_update(struct Lights* lights, struct Floorpanel floorpanel) {
-    if (floorpanel.BTN_FLR_1_UP == 1) {
-        lights->BTN_FLR_1_UP =
+void Lights_update(struct Floorpanel floorpanel, struct Elevator elevator) {
+    //Bestillingsknappene oppover
+    if (floorpanel.BTN_FLR_1_UP == true) {
+        elevio_buttonLamp(0, BUTTON_HALL_UP, 1);
     }
+    if (floorpanel.BTN_FLR_2_UP == true) {
+        elevio_buttonLamp(1, BUTTON_HALL_UP, 1);
+    }
+    if (floorpanel.BTN_FLR_3_UP == true) {
+        elevio_buttonLamp(2, BUTTON_HALL_UP, 1);
+    }
+
+    //Bestillingsknappene nedover
+    if (floorpanel.BTN_FLR_2_DWN == true) {
+        elevio_buttonLamp(1, BUTTON_HALL_DOWN, 1);
+    }
+    if (floorpanel.BTN_FLR_3_DWN == true) {
+        elevio_buttonLamp(2, BUTTON_HALL_DOWN, 1);
+    }
+    if (floorpanel.BTN_FLR_4_DWN == true) {
+        elevio_buttonLamp(3, BUTTON_HALL_DOWN, 1);
+    }
+    
+    //Panellysene
+    if (elevator.BTN_4 == true) {
+        elevio_buttonLamp(3, BUTTON_CAB, 1);
+    }
+    if (elevator.BTN_3 == true) {
+        elevio_buttonLamp(2, BUTTON_CAB, 1);
+    }
+    if (elevator.BTN_2 == true) {
+        elevio_buttonLamp(1, BUTTON_CAB, 1);
+    }
+    if (elevator.BTN_1 == true) {
+        elevio_buttonLamp(0, BUTTON_CAB, 1);
+    }
+
+    //Setter etasjeindikator
+    elevio_floorIndicator(elevator.current_floor);
+
+    //Dør åpen
+    elevio_doorOpenLamp(elevator.door_is_open);
+
+    //Stoppknapp
+    elevio_stopLamp(elevator.stop_button_pressed);
 }
 
 
-void Lights_remove_lights(struct Lights* lights, struct Elevator elevator) {
+void Lights_remove_lights(struct Elevator elevator) {
     if (elevator.current_floor == 0) {
-        lights->BTN_FLR_1_UP = 0;
-        lights->BTN_1 = 0;
+        elevio_buttonLamp(0, BUTTON_CAB, 0);
+        elevio_buttonLamp(0, BUTTON_HALL_UP, 0);
     } else if (elevator.current_floor == 1) {
-        lights->BTN_2 = 0;
-        lights->BTN_FLR_2_UP = 0;
-        lights->BTN_FLR_2_DWN = 0;
+        elevio_buttonLamp(1, BUTTON_CAB, 0);
+        elevio_buttonLamp(1, BUTTON_HALL_DOWN, 0);
+        elevio_buttonLamp(1, BUTTON_HALL_UP, 0);
     } else if (elevator.current_floor == 2) {
-        lights->BTN_3 = 0;
-        lights->BTN_FLR_3_UP = 0;
-        lights->BTN_FLR_3_DWN = 0;
+        elevio_buttonLamp(2, BUTTON_CAB, 0);
+        elevio_buttonLamp(2, BUTTON_HALL_DOWN, 0);
+        elevio_buttonLamp(2, BUTTON_HALL_UP, 0);
     } else if (elevator.current_floor == 3) {
-        lights->BTN_4 = 0;
-        lights->BTN_FLR_4_DWN = 0;
+        elevio_buttonLamp(3, BUTTON_CAB, 0);
+        elevio_buttonLamp(3, BUTTON_HALL_DOWN, 0);
     }
+}
+
+void Lights_turn_all_lights_off() {
+    //Bestillingsknapp
+    elevio_buttonLamp(0, BUTTON_HALL_UP, 0); //Setter bestillingsknapplys i første etasje til 0, altså av
+    elevio_buttonLamp(1, BUTTON_HALL_DOWN, 0);
+    elevio_buttonLamp(1, BUTTON_HALL_UP, 0);
+    elevio_buttonLamp(2, BUTTON_HALL_DOWN, 0);
+    elevio_buttonLamp(2, BUTTON_HALL_UP, 0);
+    elevio_buttonLamp(3, BUTTON_HALL_DOWN, 0);
+    elevio_buttonLamp(3, BUTTON_HALL_UP, 0);
+    elevio_buttonLamp(4, BUTTON_HALL_DOWN, 0);
+
+    //Etasjelysene settes med elevio_floorIndicator(int floor);
+
+    //Panellysene
+    elevio_buttonLamp(0, BUTTON_CAB, 0);
+    elevio_buttonLamp(1, BUTTON_CAB, 0);
+    elevio_buttonLamp(2, BUTTON_CAB, 0);
+    elevio_buttonLamp(3, BUTTON_CAB, 0);
+
+    //Dør åpen lys
+    elevio_doorOpenLamp(0);
 }
--- skeleton_project/source/driver/Lights.h
+++ skeleton_project/source/driver/Lights.h
@@ -1,40 +1,20 @@
 #pragma once
 #include "Elevator.h"
 #include "floorpanel.h"
+#include "elevio.h"
 
 //Trenger vi disse boolsa eller trenger vi bare å sjekke fra floorpanel også sette lysene høyt etter dem?
 //Så kan vi skru av lys når vi ankommer etasjer/ stoppknappen trykkes
-struct Lights {
-    bool BTN_FLR_4_DWN;
-    bool BTN_FLR_3_DWN;
-    bool BTN_FLR_3_UP;
-    bool BTN_FLR_2_DWN;
-    bool BTN_FLR_2_UP;
-    bool BTN_FLR_1_UP;
-
-    bool doorOpenLamp;
-    bool stopLamp;
-
-    bool floorLamp_1;
-    bool floorLamp_2;
-    bool floorLamp_3;
-    bool floorLamp_4;
-
-    bool BTN_1;
-    bool BTN_2;
-    bool BTN_3;
-    bool BTN_4;
-};
 
 //Oppdaterer alle lys
-struct Lights* Lights_init();
+void Lights_init();
 
 //Skrur på aktuelle lys
-void Lights_update(struct Lights* lights, struct Floorpanel floorpanel);
+void Lights_update(struct Floorpanel floorpanel, struct Elevator elevator);
 
 //Fjerner lys i etasjen man ankommer
-void Lights_remove_lights(struct Lights* lights, struct Elevator elevator);
+void Lights_remove_lights(struct Elevator elevator);
 
-//Skrur av alle lys i tilfelle stopp
+//Skrur av alle lys i tilfelle stopp (untatt stoppknappen)
 void Lights_turn_all_lights_off();
 
