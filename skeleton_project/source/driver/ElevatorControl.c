8bd28ad49940e2b18c463f6a0c795652d2fa143c
Johannes Daniel Selnes <johands@stud.ntnu.no>
2026-03-09 18:43:09 +0100

    updated light in moving state

--- skeleton_project/source/driver/ElevatorControl.c
+++ skeleton_project/source/driver/ElevatorControl.c
@@ -109,7 +109,9 @@ void ElevatorControl_run(struct ElevatorControl* elevator_control){
             q_system_remove_FLR(elevator_control->q_system, elevator_control->Elevator);
             Lights_remove_lights(*elevator_control->Elevator);
             Elevator_update(elevator_control->Elevator);
-            
+            Floorpanel_update(elevator_control->floorpanel);
+            Lights_remove_lights(*elevator_control->Elevator);
+
             Elevator_set_door(elevator_control->Elevator, true);
             Lights_update(*elevator_control->floorpanel, *elevator_control->Elevator);
             Elevator_set_door(elevator_control->Elevator, false);
